b6b794324c1c036811c8167686ab0aeac9c77075
Benjamin Klein <ben@silver-chalice.com>
2013-07-15 15:47:50 -0500

    clean up & organization &c.

--- rizzo.groovy
+++ rizzo.groovy
@@ -1,7 +1,7 @@
 import java.text.SimpleDateFormat
 import groovy.text.SimpleTemplateEngine
 
-//yay definitions!
+//Define all the things
 def root = new File("/Users/ebenezer/Works/elliotquotes/")
 def published = new File("$root/published/")
 published.mkdirs()
@@ -9,9 +9,12 @@ def homeTemplate = new File("$root/templates/home.html")
 def postTemplate = new File("$root/templates/post.html")
 def archivesTemplate = new File("$root/templates/archives.html")
 def archivesEntryTemplate = new File("$root/templates/arc_entry.html")
+def posts = []
+def engine = new SimpleTemplateEngine()
 SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyyy h:mm a")
 SimpleDateFormat archiveFormatter = new SimpleDateFormat("MMMMM d, yyyy")
 
+//lastUpdated is not used yet; nonetheless
 class Post {
     String title
     String content
@@ -21,9 +24,7 @@ class Post {
     Date lastUpdated
 }
 
-def posts = []
-def engine = new SimpleTemplateEngine()
-
+//Read post files
 new File("$root/posts/").eachFile { file ->
     List postText = file.readLines()
     def name = file.name[0 .. file.name.lastIndexOf('.')-1]
@@ -32,29 +33,25 @@ new File("$root/posts/").eachFile { file ->
     posts << new Post(title:postText[0], name:name, content: postText[3..-1].join("\n"), dateWritten: dateWritten, dateCreated: postDate, lastUpdated: postDate)
 }
 
-println "and the posts are $posts"
-
-posts.sort { it.dateCreated }.reverse()
-
+//Create the home page
 new File("$published/index.html").write("${engine.createTemplate(homeTemplate).make(["postTitle": posts.last().title, "postName": posts.last().name, "postDateWritten": archiveFormatter.format(posts.last().dateWritten), "postContent": posts.last().content])}")
 
-posts.each { post ->
+//Create each post page
+posts.sort { it.dateCreated }.reverse().each { post ->
     new File("$published/${post.name}/").mkdirs()
     new File("$published/${post.name}/index.html").write("${engine.createTemplate(postTemplate).make(["postTitle": post.title, "postDateWritten": archiveFormatter.format(post.dateWritten), "postContent": post.content])}")
 }
 
+//Create the archives page
 new File("$published/archives/").mkdirs()
 def archiveContent = ""
-
 posts.sort { it.dateCreated }.reverse().each { post ->
     archiveContent += "${engine.createTemplate(archivesEntryTemplate).make(["postDateCreated": archiveFormatter.format(post.dateCreated), "postName": post.name, "postTitle": post.title])}"
 }
-
 new File("$published/archives/index.html").write("${engine.createTemplate(archivesTemplate).make(["content": archiveContent])}")
 
+//Copy the CSS files
 new File("$published/css/").mkdirs()
 new AntBuilder().copy(toDir: "$published/css/") {
     fileset(dir: "css")
 }
-
-println "All done!"
